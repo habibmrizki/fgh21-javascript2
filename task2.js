bb478b1810267deb4ed31a94516d992b2efd456d
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-04 19:57:09 +0700

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,8 @@
+const names = [
+  "'Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'",
+];
+
+function searchName(parameter, limit, callback) {
+  const hasil = [];
+  for (let i = 0; i < names.length; i++) {}
+}
