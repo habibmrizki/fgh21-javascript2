1dbc217a48d3f2494ae54b663089021346b91c02
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-05 06:25:28 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,8 +1,36 @@
 const names = [
-  "'Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'",
+  "Abigail",
+  "Alexandra",
+  "Alison",
+  "Amanda",
+  "Angela",
+  "Bella",
+  "Carol",
+  "Caroline",
+  "Carolyn",
+  "Deirdre",
+  "Diana",
+  "Elizabeth",
+  "Ella",
+  "Faith",
+  "Olivia",
+  "Penelope",
 ];
 
 function searchName(parameter, limit, callback) {
-  const hasil = [];
-  for (let i = 0; i < names.length; i++) {}
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].toLowerCase().indexOf(parameter.toLowerCase()) > -1) {
+      result.push(names[i]);
+      if (result.length == limit) {
+        break;
+      }
+    }
+  }
+  callback(result);
 }
+
+function printNama(result) {
+  console.log(result);
+}
+searchName("an", 3, printNama);
