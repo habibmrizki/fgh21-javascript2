e7fd05a085c93b6a48bc7c5122229734ec1dcfc5
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-06 23:36:02 +0700

    Add files via upload

--- task3.js
+++ task3.js
@@ -2,9 +2,7 @@ const seleksiNilai = function (nilaiAwal, nilaiAkhir, dataArray) {
   if (nilaiAwal > nilaiAkhir) {
     console.log("nilai Akhir harus lebih besar dari nilaiAwal");
   }
-  if (dataArray.length < 5) {
-    console.log("Jumlah angka dalam dataArray harus lebih 5");
-  }
+
   if (dataArray.length <= 5) {
     console.log("Jumlah angka dalam dataArray harus lebih dari 5");
   }
@@ -35,3 +33,6 @@ const seleksiNilai = function (nilaiAwal, nilaiAkhir, dataArray) {
   console.log(result);
 };
 seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
+seleksiNilai(15, 3, [2, 25, 4, 14, 17, 38, 8]);
+seleksiNilai(4, 17, [2, 25, 4]);
+seleksiNilai(5, 17, [2, 25, 4, 1, 30, 18]);
