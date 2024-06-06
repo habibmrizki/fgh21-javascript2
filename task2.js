f060bf59df0a5c5e98e63b867584168666f3953c
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-06 22:20:42 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,36 +1,149 @@
-const names = [
-  "Abigail",
-  "Alexandra",
-  "Alison",
-  "Amanda",
-  "Angela",
-  "Bella",
-  "Carol",
-  "Caroline",
-  "Carolyn",
-  "Deirdre",
-  "Diana",
-  "Elizabeth",
-  "Ella",
-  "Faith",
-  "Olivia",
-  "Penelope",
-];
+const searchName = function (keyword, limit, cb) {
+  const names = [
+    "Abigail",
+    "Alexandra",
+    "Alison",
+    "Amanda",
+    "Angela",
+    "Bella",
+    "Carol",
+    "Caroline",
+    "Carolyn",
+    "Deirdre",
+    "Diana",
+    "Elizabeth",
+    "Ella",
+    "Faith",
+    "Olivia",
+    "Penelope",
+  ];
 
-function searchName(parameter, limit, callback) {
-  let result = [];
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let searchResult = [];
+
   for (let i = 0; i < names.length; i++) {
-    if (names[i].toLowerCase().indexOf(parameter.toLowerCase()) > -1) {
-      result.push(names[i]);
-      if (result.length == limit) {
-        break;
+    const personName = names[i];
+    let result = "";
+
+    for (let i = 0; i < personName.length; i++) {
+      let convert = false;
+      for (let j = 0; j < uppercase.length; j++) {
+        if (convert === false) {
+          if (personName[i] === uppercase[j]) {
+            result += lowercase[j];
+            convert = true;
+          }
+
+          if (j === 25) {
+            if (personName[i] !== uppercase[j]) {
+              result += personName[i];
+              convert = true;
+            }
+          }
+        }
       }
     }
-  }
-  callback(result);
-}
 
-function printNama(result) {
+    let isFound = false;
+    for (let i = 0; i < result.length; i++) {
+      let segment = "";
+      for (let j = i; j < keyword.length + i; j++) {
+        if (result[j] !== undefined) {
+          segment += result[j];
+        }
+      }
+      if (segment === keyword) {
+        isFound = true;
+      }
+    }
+
+    if (isFound === true) {
+      searchResult = [...searchResult, personName];
+    }
+  }
+  cb(searchResult, limit);
+};
+
+function limitResult(arr, lim) {
+  let result = [];
+  for (let i = 0; i < lim; i++) {
+    if (arr[i]) {
+      result = [...result, arr[i]];
+    }
+  }
   console.log(result);
 }
-searchName("an", 3, printNama);
+
+searchName("an", 2, limitResult);
+searchName("ca", 2, limitResult);
+
+// const names = [
+//   "Abigail",
+//   "Alexandra",
+//   "Alison",
+//   "Amanda",
+//   "Angela",
+//   "Bella",
+//   "Carol",
+//   "Caroline",
+//   "Carolyn",
+//   "Deirdre",
+//   "Diana",
+//   "Elizabeth",
+//   "Ella",
+//   "Faith",
+//   "Olivia",
+//   "Penelope",
+// ];
+
+// const keyword = "an";
+// const limit = 3;
+
+// const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowercase = "abcdefghijklmnopqrstuvwxyz";
+// let searchResult = [];
+
+// const
+
+// for (let i = 0; i < names.length; i++) {
+//   const personName = names[i];
+//   let result = "";
+
+//   for (let i = 0; i < personName.length; i++) {
+//     let convert = false;
+//     for (let j = 0; j < uppercase.length; j++) {
+//       if (convert === false) {
+//         if (personName[i] === uppercase[j]) {
+//           result += lowercase[j];
+//           convert = true;
+//         }
+
+//         if (j === 25) {
+//           if (personName[i] !== uppercase[j]) {
+//             result += personName[i];
+//             convert = true;
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   let isFound = false;
+//   for (let i = 0; i < result.length; i++) {
+//     let segment = "";
+//     for (let j = i; j < keyword.length + i; j++) {
+//       if (result[j] !== undefined) {
+//         segment += result[j];
+//       }
+//     }
+//     if (segment === keyword) {
+//       isFound = true;
+//     }
+//   }
+
+//   if (isFound === true) {
+//     searchResult = [...searchResult, personName];
+//   }
+// }
+// console.log(searchResult);
--- task3.js
+++ task3.js
@@ -1,16 +1,37 @@
-function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
-  if (nilaiAwal < nilaiAkhir) {
-    if (dataArray.length > 5) {
-      let container = [];
-      for (let i = 0; i < dataArray.length; i++) {
-        if (dataArray[i] > nilaiAwal && dataArray[i] < nilaiAkhir) {
-          container.push(dataArray[i]);
-          container.sort((a, b) => a - b);
-        }
+const seleksiNilai = function (nilaiAwal, nilaiAkhir, dataArray) {
+  if (nilaiAwal > nilaiAkhir) {
+    console.log("nilai Akhir harus lebih besar dari nilaiAwal");
+  }
+  if (dataArray.length < 5) {
+    console.log("Jumlah angka dalam dataArray harus lebih 5");
+  }
+  if (dataArray.length <= 5) {
+    console.log("Jumlah angka dalam dataArray harus lebih dari 5");
+  }
+
+  let result = [];
+  for (let i = 0; i < dataArray.length; i++) {
+    const num = dataArray[i];
+    if (num > nilaiAwal) {
+      if (num < nilaiAkhir) {
+        result = [...result, num];
       }
-      console.log(container);
     }
   }
-}
-const result = seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
-console.log(result);
+  if (result.length === 0) {
+    console.log("Nilai tidak ditemukan");
+  } else {
+    let len = result.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (result[j] > result[j + 1]) {
+          let temp = result[j];
+          result[j] = result[j + 1];
+          result[j + 1] = temp;
+        }
+      }
+    }
+  }
+  console.log(result);
+};
+seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
